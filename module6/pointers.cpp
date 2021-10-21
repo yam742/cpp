21e940c89b335f503744cd2166c335b963f7ab34
Yuriy Makhin <yam742@yanex.ru>
2021-10-21 11:10:10 +0300

    pointers.cpp

---
+++ module6/pointers.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	float* pF;
+       	double* pD;
+	int* pI;
+	char* pC;
+
+		
+	float f = 3.14f ;
+       	double d = 2.75;
+	int i = 7;
+	char c = 'A';
+	
+	pF = &f;
+       	pD = &d;
+    	pI = &i;
+	pC = &c;
+
+	printf("f = %f, *pF = %f\n", f, *pF);
+	printf("d = %f, *pD = %f\n", d, *pD);
+	printf("i = %i, *pI = %i\n", i, *pI);
+	printf("c = %c, *pC = %c\n", c, *pC);
+
+	*pF = 1.66f;
+       	*pD = 9.8;
+    	*pI = 365;
+	*pC = 'X';
+
+	printf("f = %f, *pF = %f\n", f, *pF);
+	printf("d = %f, *pD = %f\n", d, *pD);
+	printf("i = %i, *pI = %i\n", i, *pI);
+	printf("c = %c, *pC = %c\n", c, *pC);
+}	
